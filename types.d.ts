37a3d91e385ba8c198e5975e3600d72fb8c0eb50
Re:Gelu <morandva@yandex.ru>
2023-06-29 01:43:38 +0300

    Added type FaunaResponse

--- types.d.ts
+++ types.d.ts
@@ -7,9 +7,10 @@ type Choice = {
 
 // Тип для опроса
 type Poll = {
-  id: number,
-  question: string,
+  id: string;
+  question: string;
   choices: Choice[];
+  is_multiple_answer_options: boolean;
   created_at: Date;
   expires_at?: string;
 }
@@ -19,6 +20,18 @@ type Response = {
   id: string;
   poll_id: string;
   choice_id: string;
-  voter_id: string;
+  voter_ip: string;
   created_at: string;
+};
+
+// Type of Fauna DB response
+type FaunaResponse = {
+  ref: { id: string };
+  data: object;
+  ts: number;
+};
+
+// Type of Fauna DB query response
+type FaunaQueryResponse = {
+  data: FaunaResponse[]
 };

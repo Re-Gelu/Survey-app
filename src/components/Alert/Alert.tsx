042b0a23cce5779063c88715bc59734e11b0b4bd
Re:Gelu <morandva@yandex.ru>
2023-06-29 17:26:01 +0300

    Fixed Alert component props

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,11 +1,7 @@
 import { Alert } from '@mantine/core';
 import { IconAlertCircle } from '@tabler/icons-react';
 
-interface CustomAlertProps {
-  children: React.ReactNode;
-}
-
-export const CustomAlert = ({children}: CustomAlertProps) => {
+export const CustomAlert = ({children}: React.PropsWithChildren) => {
     return (
         <Alert icon={<IconAlertCircle size="2rem" />} title="Oh no... Cringe!" color="red" variant="outline">
             {children}

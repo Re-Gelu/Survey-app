ad06b0be90bf39f8876333d78f7e5f8f2b22d2c1
Re:Gelu <morandva@yandex.ru>
2023-07-01 19:43:58 +0300

    Fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,14 @@ import requestIp from 'request-ip';
 
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const res = NextResponse.next();
-  const ip = requestIp.getClientIp(request);
+  const ip = requestIp.getClientIp(request as any);
+  const nextIp = request.ip;
 
-  if (ip) {
-    res.cookies.set("user-ip", ip, {
-      httpOnly: false,
-    });
-  };
+  res.cookies.set(
+    "user-ip", 
+    nextIp ? nextIp : ip ? ip : "", 
+    { httpOnly: false }
+  );
   
   return res;
 };

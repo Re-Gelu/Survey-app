511f85e5794226ce892afa3b025b66ee1a373da6
Re:Gelu <morandva@yandex.ru>
2023-07-01 22:11:51 +0300

    Custom error pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,22 @@
+import { Center, Stack, Title, Paper, Container } from '@mantine/core';
+
+const Custom500 = () => {
+  return (
+    <Container my="xl" py="xl">
+      <Paper shadow="sm" radius="md" p="xl" withBorder>
+        <Center my="xl" py="xl">
+          <Stack align="center"> 
+            <Title order={2} fw={200}>
+              A server error has occurred...
+            </Title>
+            <Title order={2} fw={200}>
+              Try to come back later :3
+            </Title>
+          </Stack>
+        </Center>
+      </Paper>
+    </Container>
+  )
+};
+
+export default Custom500;

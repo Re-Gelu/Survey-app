510742810cd199bcee8d777e9cdc8e9d7abc5077
Re:Gelu <morandva@yandex.ru>
2023-07-02 20:04:06 +0300

    Added multiple voting func

--- src/pages/polls/[id].tsx
+++ src/pages/polls/[id].tsx
@@ -3,24 +3,24 @@ import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import { Grid, Text, Center, Loader, Space, Title, Container, Radio, Button, 
-  Group, Box, Progress, Transition, CopyButton, ActionIcon, Tooltip } from '@mantine/core';
+  Group, Box, Progress, Transition, CopyButton, ActionIcon, Tooltip, Checkbox } from '@mantine/core';
 import { CustomAlert } from '@/components/Alert/Alert';
-import { useForm, isNotEmpty } from '@mantine/form';
+import { useForm, isNotEmpty, TransformedValues } from '@mantine/form';
 import { useCounter } from '@mantine/hooks';
 import { notifications } from '@mantine/notifications';
-import { IconShare, IconCheck, IconExclamationMark, IconSkull, } from '@tabler/icons';
+import { IconShare, IconCheck, IconExclamationMark, IconSkull } from '@tabler/icons';
 import { getCookie } from 'cookies-next';
 import requestIp from 'request-ip';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 import fetcher from '@/swr';
 import axios from 'axios';
 
 const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  const { data, error, isLoading } = useSWR(`/api/polls/${router.query.id}`, fetcher);
+  const { data, error, isLoading  } = useSWR(`/api/polls/${router.query.id}`, fetcher);
   const [ votesAmount, votesAmountHandlers ] = useCounter(0, {min: 0, max: 10000});
   const [ isAlreadyVoted, setIsAlreadyVoted ] = useState<boolean>(false)
-  const [ votedOption, setVoteOption ] = useState<string>("");
+  const [ votedOption, setVoteOption ] = useState<string []>([]);
   const [ isAnimate, setIsAnimate ] = useState<boolean>(false);
 
   // Getting amount of votes, picked vote option and is already voted value
@@ -28,12 +28,22 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
     if (!error && !isLoading && data) {
       votesAmountHandlers.set(data.choices.reduce((sum: number, choice: Choice) => sum + choice.votes.length, 0));      
       setIsAlreadyVoted(data.choices.some((choice: Choice) => choice.votes.some((vote: Vote) => vote.voter_ip === props.ip)));
-      const votedChoice: Choice = data.choices.find((choice: Choice) => choice.votes.find((vote: Vote) => vote.voter_ip === props.ip));
-      if (isAlreadyVoted && votedChoice) {
-        setVoteOption(votedChoice.text);
-      }
     };
   }, [data, error, isLoading]);
+
+  // Setting vote options if already voted
+  useEffect(() => {
+    if (!error && !isLoading && data) {
+      //const votedChoice: Choice = data.choices.find((choice: Choice) => choice.votes.find((vote: Vote) => vote.voter_ip === props.ip));
+      const votedChoice: string[] = data.choices.filter((choice: Choice) =>
+        choice.votes.some((vote: Vote) => vote.voter_ip === props.ip)
+      )
+      .map((choice: Choice) => choice.text);
+      if (isAlreadyVoted && votedChoice) {
+        setVoteOption(votedChoice);
+      };
+    };
+  }, [data, error, isLoading, isAlreadyVoted]);
 
   // Animate votes progressbars
   useEffect(() => {
@@ -45,23 +55,27 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
   // If voted - set value
   useEffect(() => {
     form.setFieldValue(
-      'choice', votedOption
+      'choices', (votedOption.length == 1) ? votedOption[0] : votedOption
     );
   }, [votedOption]);
   
   
   const form = useForm({
     initialValues: {
-      choice: votedOption,
+      choices: votedOption as string | string[],
     },
 
+    transformValues: (values) => ({
+      choices: (Array.isArray(values.choices)) ? [...values.choices]: [values.choices],
+    }),
+
     validate: {
-      choice: isNotEmpty(),
+      choices: isNotEmpty(),
     },
   });
 
   const handleError = (errors: typeof form.errors) => {
-    if (errors.choice) {
+    if (errors.choices) {
       notifications.show({ 
         message: 'Please enter the answer', 
         color: 'red',
@@ -70,13 +84,16 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
     };
   };
 
-  const handleSubmit = async (values: typeof form.values, event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (values: TransformedValues<typeof form>, event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    axios.post(`/api/polls/${router.query.id}/vote`, {choice_text: values.choice})
+    axios.post(`/api/polls/${router.query.id}/vote`, values)
     .then(() => {
       // Update votes amount and is already voted value
       votesAmountHandlers.increment();
       setIsAlreadyVoted(true);
+
+      // Refresh SWR data to show actual votes
+      mutate(`/api/polls/${router.query.id}`);
 
       notifications.show({ 
         message: 'Successfully voted', 
@@ -103,15 +120,27 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
   // Create radio buttons to vote
   const voting_options = ((!error && !isLoading && data) ? 
       (data.is_multiple_answer_options) ?
-        (data.choices.map((item: Choice) => (
-          <Radio.Group name="pollChoices">
-            {item.text}
-          </Radio.Group>
-        )))
+        /* If multiple answer options */
+        <Checkbox.Group
+          defaultValue={votedOption}
+          {...form.getInputProps('choices')}
+        >
+            {data.choices.map((item: Choice) => (
+              <Checkbox
+                key={item.text} 
+                label={item.text} 
+                value={item.text} 
+                size="md" 
+                mt="lg" 
+              >
+              </Checkbox>
+            ))}
+        </Checkbox.Group>
       : 
+        /* If not multiple answer options */
         <Radio.Group 
-          name="choice"
-          {...form.getInputProps('choice')}
+          name="choices"
+          {...form.getInputProps('choices')}
         >
             {data.choices.map((item: Choice) => (
               <Radio
@@ -120,7 +149,6 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
                 value={item.text} 
                 size="md" 
                 mt="lg" 
-                checked
               >
               </Radio>
             ))}
@@ -133,11 +161,29 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
   const voting_results = ((!error && !isLoading && data) ? 
       (isAlreadyVoted) ? 
         (data.is_multiple_answer_options) ?
-          <></>
-        :
+          /* If multiple answer options */
+          <Checkbox.Group 
+            {...form.getInputProps('choices')}
+          >
+            {data.choices.map((item: Choice) => (
+              <Group key={item.text} grow mt="lg">
+                <Checkbox  
+                  label={item.text} 
+                  value={item.text} 
+                  size="md"
+                >
+                </Checkbox>
+                <Transition mounted={isAnimate} keepMounted transition="fade" duration={500}>
+                  {(styles) => <Progress style={styles} label={`${item.votes.length}`} size="xl" value={(item.votes.length / votesAmount) * 100} />}
+                </Transition>
+              </Group>
+            ))}
+          </Checkbox.Group>
+        : 
+          /* If not multiple answer options */
           <Radio.Group 
-            name="choice"
-            {...form.getInputProps('choice')}
+            name="choices"
+            {...form.getInputProps('choices')}
           >
             {data.choices.map((item: Choice) => (
               <Group key={item.text} grow mt="lg">
@@ -174,16 +220,24 @@ const PollPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>)
               </Head>
 
               <Grid my="xl" align="center" justify="space-between">
-                <Grid.Col sm={9} xs={12}>
+                <Grid.Col sm={6} xs={12}>
                   <Title order={2} fw={400}>
                     Current poll theme: 
                     <Text inherit fw={200} variant="gradient" component="span" mx="sm">{data.question && data.question}</Text>
                   </Title>
                 </Grid.Col>
                 <Grid.Col span="content">
-                  <Group>
+                  <Group spacing="xs">
                     <Text fs="italic" fz="lg" c="dimmed">
                       {data.created_at && new Date(data.created_at).toLocaleDateString()}
+                      {data.expires_at &&  
+                        <Text component='span' pl={6} fs="italic" fz="lg" c="dimmed">
+                          - {`
+                            ${new Date(data.expires_at).toLocaleDateString()}
+                            ${new Date(data.expires_at).toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' })}
+                          `}
+                        </Text>
+                      }
                     </Text>
                     <CopyButton value={window.location.href}>
                       {({ copied, copy }) => (

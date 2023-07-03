5ca15955d6245d301ac07785a54c736b8d000d57
Re:Gelu <morandva@yandex.ru>
2023-07-03 16:21:18 +0300

    Small code refactoiring

--- src/components/PollsTable/PollsTable.tsx
+++ src/components/PollsTable/PollsTable.tsx
@@ -8,30 +8,30 @@ export const PollsTable = ({ data }: { data: Poll[] }) => {
   
   const rows = data && data.map((item) => (
     <tr key={item.id && item.id}>
-        <td>
-          <Link href={`/polls/${item.id}`}>
-            <Grid align="center" justify="space-between">
-              <Grid.Col span="content" p={0}>
-                <ActionIcon variant="transparent" className={classes.adapt}><IconArrowBadgeRight size={28} /></ActionIcon>
-              </Grid.Col>
-              <Grid.Col span="auto">
-                <Text fz="xl" fw={400} variant="gradient" className={classes.adaptFont}>
-                  {item.question && item.question}
-                </Text>
-              </Grid.Col>
-            </Grid>
-          </Link>
-        </td>
-        <td>{item.created_at && new Date(item.created_at).toLocaleDateString()}</td>
-        <td>
-          {item.expires_at ? (new Date(item.expires_at).getTime() >= new Date().getTime()) ? 
-                <Badge fullWidth>Active</Badge>
-              : 
-                <Badge color="gray" fullWidth>Finished</Badge>
-            : 
+      <td>
+        <Link href={`/polls/${item.id}`}>
+          <Grid align="center" justify="space-between">
+            <Grid.Col span="content" p={0}>
+              <ActionIcon variant="transparent" className={classes.adapt}><IconArrowBadgeRight size={28} /></ActionIcon>
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <Text fz="xl" fw={400} variant="gradient" className={classes.adaptFont}>
+                {item.question && item.question}
+              </Text>
+            </Grid.Col>
+          </Grid>
+        </Link>
+      </td>
+      <td>{item.created_at && new Date(item.created_at).toLocaleDateString()}</td>
+      <td>
+        {item.expires_at ? (new Date(item.expires_at).getTime() >= new Date().getTime()) ? 
               <Badge fullWidth>Active</Badge>
-          }
-        </td>
+            : 
+              <Badge color="gray" fullWidth>Finished</Badge>
+          : 
+            <Badge fullWidth>Active</Badge>
+        }
+      </td>
     </tr>
   ));
 
